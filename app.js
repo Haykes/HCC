2ad7c175a95610c01723363debbf38c2fc98c949
Haykes <haykes.pro@gmail.com>
2023-04-05 11:50:36 +0200

    Template Modifié + Routes modifé

--- app.js
+++ app.js
@@ -13,12 +13,6 @@ app.use(express.json());
 
 const db = new sqlite3.Database(":memory:");
 
-// Création des tables et ajout de données initiales
-db.serialize(function () {
-  // Ajoutez ici les instructions SQL pour créer vos tables
-  // Exemple : db.run("CREATE TABLE adherents (id INTEGER PRIMARY KEY AUTOINCREMENT, nom TEXT, prenom TEXT, ...)");
-});
-
 app.use("/auth", authRoutes);
 app.use("/adherents", adherentsRoutes);
 app.use("/actualites", actualitesRoutes);
